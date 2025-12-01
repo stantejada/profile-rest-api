67c8fc8b561aab71f5a2212b2514d267d566e2cd
Stanley <stanleytejadaj@icloud.com>
2025-12-01 15:52:31 -0800

    Added handle for login serializers

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from profiles_api import models
 
+from django.contrib.auth import authenticate
+
 class HelloSerializers(serializers.Serializer):
     """ Serializes a name field for testing our APIView"""
 
@@ -36,3 +38,37 @@ class UserProfileSerializer(serializers.ModelSerializer):
             password = validated_data.pop('password')
             instance.set_password(password)
         return super().update(instance, validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    """ Handle User login with email """
+    email = serializers.EmailField()
+    password = serializers.CharField(
+    style={'input_type': 'password'},
+    trim_whitespace = False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            # DRF's authenticate() still expects "username" map it to email
+            user = authenticate(
+            request = self.context.get('request'),
+            username = email,
+            password = password
+            )
+
+            if not user:
+                raise serializers.ValidationError(
+                'Unable to log in with provided credentials',
+                code = 'authorization'
+                )
+
+        else:
+            raise serializers.ValidationError(
+            'Must include "email" and "password".',
+            code = 'authorization'
+            )
+        attrs['user'] = user
+        return attrs
